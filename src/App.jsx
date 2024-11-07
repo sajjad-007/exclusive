8c9188e6a025348e01608c91a5ae9b8dd0c4df69
sajjad-007 <sajjadhossain8123@gmail.com>
2024-11-07 17:04:56 +0600

    first commit tailwind css and react router dom set up

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import {
+  createRoutesFromElements,
+  createBrowserRouter,
+  Route,
+  RouterProvider,
+} from "react-router-dom";
+import Home from "./pages/home/Home";
+import About from "./pages/about/About";
+
+function App() {
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <Route>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/about" element={<About/>}/>
+      </Route>
+    )
+  );
+  return (
+    <>
+      <RouterProvider router={router}/>
+    </>
+  )
+}
+
+export default App
